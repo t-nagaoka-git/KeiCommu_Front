6d63abdc65c5547f96b5a36a843e223ab4694b84
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-30 20:39:06 +0900

    Fix: トップページにログインユーザー情報を出力する

--- app/src/components/templates/index.tsx
+++ app/src/components/templates/index.tsx
@@ -1,3 +1,19 @@
+import {useContext} from 'react';
+import {AuthContext} from '@/pages/_app';
+
 export function Template() {
-  return <h1>ログイン専用</h1>;
+  const {isLoggedIn, currentUser} = useContext(AuthContext);
+  return (
+    <>
+      {isLoggedIn && currentUser ? (
+        <>
+          <h1>Signed in successfully!</h1>
+          <h2>Email: {currentUser?.email}</h2>
+          <h2>Name: {currentUser?.name}</h2>
+        </>
+      ) : (
+        <h1>Not signed in</h1>
+      )}
+    </>
+  );
 }
