349a7e46c6cc7e176a54e85fcbc5c909a426789a
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-30 20:25:09 +0900

    Remove: GetStaticProps を削除する

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,23 +1,6 @@
-import {GetStaticProps} from 'next';
-import {useRouter} from 'next/router';
-import {useEffect, useContext} from 'react';
-import {AuthContext} from '@/pages/_app';
 import {Template} from '@/components/templates';
 
-export const getStaticProps: GetStaticProps = async () => {
-  return {props: {}};
-};
-
 export default function Page() {
-  const router = useRouter();
-  const {isLoggedIn} = useContext(AuthContext);
-  useEffect(() => {
-    console.log(isLoggedIn);
-    if (!isLoggedIn) {
-      router.push('/login');
-    }
-  }, []);
-
   return (
     <>
       <Template />
--- app/src/pages/signup/index.tsx
+++ app/src/pages/signup/index.tsx
@@ -1,9 +1,4 @@
-import {GetStaticProps} from 'next';
 import {Template} from '@/components/templates/signup';
-
-export const getStaticProps: GetStaticProps = async () => {
-  return {props: {}};
-};
 
 export default function Page() {
   return (
