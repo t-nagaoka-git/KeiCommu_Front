d69b4d1562224692d556d5b0853d610d0c23a260
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-30 20:36:26 +0900

    Add: ヘッダーに AuthButtons コンポーネントを追加する

--- app/src/components/organisms/Header/Nav/Nav.tsx
+++ app/src/components/organisms/Header/Nav/Nav.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link';
+import AuthButtons from '@/components/molecules/AuthButtons';
 import styles from './styles.module.css';
 
 export function Nav() {
-
   return (
     <nav className={styles.module}>
+      <AuthButtons />
     </nav>
   );
 }
