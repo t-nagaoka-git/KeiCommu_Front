3913682bfbeacda7406b0f67b41cc78c7368f060
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-02 10:00:41 +0900

    Fix: ログイン画面のデザインを整える

--- app/src/components/templates/login/index.tsx
+++ app/src/components/templates/login/index.tsx
@@ -1,16 +1,33 @@
+import {makeStyles, Theme} from '@material-ui/core/styles';
 import {useContext, useState} from 'react';
 import {AuthContext} from '@/pages/_app';
 import {SignInParams} from '@/interfaces/index';
 import {signIn} from '@/apis/auth';
 import Cookies from 'js-cookie';
 import {useRouter} from 'next/router';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import Link from 'next/link';
-import {InputText} from '@/components/atoms/InputText';
-import {Button} from '@/components/atoms/Button';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import AlertMessage from '@/components/atoms/AlertMessage';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  btn: {
+    marginTop: theme.spacing(1),
+  },
+  card: {
+    margin: theme.spacing(1),
+    padding: theme.spacing(2),
+    maxWidth: 500,
+  },
+}));
 
 export function Template() {
   const {isLoggedIn, setIsLoggedIn, setCurrentUser} = useContext(AuthContext);
+  const classes = useStyles();
   const router = useRouter();
 
   if (isLoggedIn) router.push('/');
@@ -55,23 +72,57 @@ export function Template() {
   return (
     <>
       <form noValidate autoComplete="off">
-        <div>
-          <p>アカウントをお持ちでない方はこちら</p>
-          <Link href={`/signup`}>新規会員登録</Link>
-          <InputText
-            type="text"
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-          />
-          <InputText
-            type="password"
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-          <Button onClick={handleSubmit}>ログイン</Button>
-        </div>
+        <Card className={classes.card}>
+          <CardContent>
+            <Box textAlign="center">
+              <Typography variant="body2">アカウントをお持ちでない方はこちら</Typography>
+            </Box>
+            <Link href={`/signup`}>
+              <Button variant="contained" size="large" fullWidth color="primary" className={classes.btn}>
+                新規会員登録
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
+        <Card className={classes.card}>
+          <CardContent>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="メールアドレス"
+              value={email}
+              margin="dense"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="パスワード"
+              type="password"
+              value={password}
+              margin="dense"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              fullWidth
+              color="primary"
+              disabled={!email || !password ? true : false}
+              className={classes.btn}
+              onClick={handleSubmit}
+            >
+              ログイン
+            </Button>
+          </CardContent>
+        </Card>
       </form>
       <AlertMessage
         open={alertMessageOpen}
