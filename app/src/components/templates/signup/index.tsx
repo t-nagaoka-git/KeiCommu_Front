76e07a7815bed3278342a06a7361199c5e139ad3
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-03 09:53:45 +0900

    Fix: アカウント作成画面のデザインを整える

--- app/src/components/templates/signup/index.tsx
+++ app/src/components/templates/signup/index.tsx
@@ -1,17 +1,44 @@
+import {makeStyles, Theme} from '@material-ui/core/styles';
 import React from 'react';
 import Cookies from 'js-cookie';
 import {signUp} from '@/apis/auth';
 import {SignUpParams} from '@/interfaces/index';
-import {Button} from '@/components/atoms/Button';
-import {InputText} from '@/components/atoms/InputText';
-import {InputRadio} from '@/components/atoms/InputRadio';
-import {InputPassword} from '@/components/atoms/InputPassword';
+import {Card, CardContent, CardHeader, TextField, Button} from '@material-ui/core';
+import {ToggleButtonGroup, ToggleButton} from '@material-ui/lab';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    padding: theme.spacing(2),
+    maxWidth: 500,
+  },
+  header: {
+    textAlign: 'center',
+  },
+  toggle: {
+    marginTop: theme.spacing(1),
+    width: '100%',
+  },
+  toggleBtn: {
+    width: '50%',
+  },
+  submitBtn: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 export function Template() {
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [gender, setGender] = React.useState('man');
+  const classes = useStyles();
+
+  const [name, setName] = React.useState<string>('');
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  const [gender, setGender] = React.useState<string>('man');
+
+  const handleChnageGender = (e: React.MouseEvent<HTMLButtonElement>, value: any) => {
+    if (value !== null) {
+      setGender(value);
+    }
+  };
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -42,48 +69,72 @@ export function Template() {
 
   return (
     <form noValidate autoComplete="off">
-      <div>
-        <p>アカウントを作成する</p>
-        <p>名前</p>
-        <InputText
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-        <p>メールアドレス</p>
-        <InputText
-          type="text"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <p>パスワード</p>
-        <InputPassword
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-        <p>性別</p>
-        <InputRadio
-          name="gender"
-          value="man"
-          onChange={(event) => {
-            setGender(event.target.value);
-          }}
-          checked
-        />
-        男性
-        <InputRadio
-          name="gender"
-          value="woman"
-          onChange={(event) => {
-            setGender(event.target.value);
-          }}
-        />
-        女性
-        <Button onClick={handleSubmit}>アカウントを作成</Button>
-      </div>
+      <Card className={classes.card}>
+        <CardContent>
+          <CardHeader className={classes.header} title="アカウントを作成する" />
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            label="名前"
+            value={name}
+            margin="dense"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            label="メールアドレス"
+            value={email}
+            margin="dense"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            label="パスワード"
+            value={password}
+            margin="dense"
+            autoComplete="new-password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <ToggleButtonGroup
+            color="primary"
+            exclusive
+            size="small"
+            value={gender}
+            className={classes.toggle}
+            onChange={handleChnageGender}
+          >
+            <ToggleButton value="man" className={classes.toggleBtn} color="primary">
+              男性
+            </ToggleButton>
+            <ToggleButton value="woman" className={classes.toggleBtn}>
+              女性
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            fullWidth
+            color="primary"
+            disabled={!name || !email || !password || !gender ? true : false}
+            className={classes.submitBtn}
+            onClick={handleSubmit}
+          >
+            アカウントを作成
+          </Button>
+        </CardContent>
+      </Card>
     </form>
   );
 }
