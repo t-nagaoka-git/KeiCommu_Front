70c2ee2806232898f266551ea1ac0793c27bcd83
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-05 12:34:45 +0900

    Remove: posts 系のリソースを削除

--- app/src/urls/index.js
+++ app/src/urls/index.js
@@ -5,6 +5,3 @@ export const signUpUrl = 'auth';
 export const signInUrl = 'auth/sign_in';
 export const signOutUrl = 'auth/sign_out';
 export const getCurrentUserUrl = 'auth/sessions';
-
-// posts
-export const postsIndexUrl = 'posts';
