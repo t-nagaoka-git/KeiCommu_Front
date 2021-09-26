57e9ee9721890c1a52f058af236d8d9dac1794c6
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-26 12:17:39 +0900

    Fix: API レスポンスで渡されたトークンを次回の API リクエストに含めるように修正

--- app/src/lib/client.ts
+++ app/src/lib/client.ts
@@ -1,16 +1,66 @@
+import axios from 'axios';
 import {baseUrl} from '@/urls';
 import applyCaseMiddleware from 'axios-case-converter';
-import axios from 'axios';
 
 const options = {
   ignoreHeaders: true,
 };
 
-const client = applyCaseMiddleware(
-  axios.create({
-    baseURL: baseUrl,
-  }),
-  options
+const getStorage = () => {
+  return window.localStorage;
+};
+
+const authHeaderKeys = ['access-token', 'token-type', 'client', 'expiry', 'uid'];
+
+const getVerificationParams = () => {
+  const result = {};
+  const storage = getStorage();
+  authHeaderKeys.forEach((key: string) => {
+    if (storage.getItem(key)) result[key] = storage.getItem(key);
+  });
+  return result;
+};
+
+const saveHeaders = (response) => {
+  if (response && response.headers) {
+    const storage = getStorage();
+    authHeaderKeys.forEach((key: string) => {
+      if (response.headers[key]) {
+        storage.setItem(key, response.headers[key]);
+        axios.defaults.headers.common[key] = response.headers[key];
+      }
+    });
+  }
+};
+
+const instance = axios.create({
+  baseURL: baseUrl,
+});
+instance.interceptors.request.use(
+  (request) => {
+    const verificationParams = getVerificationParams();
+    request.headers = {...verificationParams};
+    console.log('Starting Request: ', request);
+    return request;
+  },
+  function (error) {
+    console.log('Request Error: ', error.response);
+    return Promise.reject(error);
+  }
 );
+instance.interceptors.response.use(
+  (response) => {
+    console.log('Response: ', response);
+    saveHeaders(response);
+    return response;
+  },
+  function (error) {
+    console.log('Response Error: ', error.response);
+    saveHeaders(error.response);
+    return Promise.reject(error);
+  }
+);
+
+const client = applyCaseMiddleware(instance, options);
 
 export default client;
