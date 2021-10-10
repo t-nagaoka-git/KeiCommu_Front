6fee63d3fbc434a8c378f420f4f8677da9fcf1a3
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-10-10 15:11:42 +0900

    Add: チーム作成用パラメータの interface を追加する

--- app/src/interfaces/index.ts
+++ app/src/interfaces/index.ts
@@ -10,20 +10,14 @@ export interface SignInParams {
   password: string;
 }
 
-export interface User {
-  id: number;
-  uid: string;
-  provider: string;
-  email: string;
-  name: string;
-  nickname?: string;
-  gender: number;
-  image?: string;
-  allowPasswordChange: boolean;
-  created_at: Date;
-  updated_at: Date;
-}
-
 export interface createMicropostParams {
   content: string;
+}
+
+export interface createTeamParams {
+  name: string;
+  description: string;
+  visibilitySetting: string;
+  genderRestriction: string;
+  autoExitGracePeriod: number;
 }
