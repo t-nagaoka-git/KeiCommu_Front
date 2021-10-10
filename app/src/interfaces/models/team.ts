566927bf469c391ad70f091a05b69fcbc6742c49
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-10-10 15:06:48 +0900

    Team 関連の interface を追加する

---
+++ app/src/interfaces/models/team.ts
@@ -0,0 +1,26 @@
+export interface Team {
+  id: number;
+  name: string;
+  description: string;
+  visibilitySetting: string;
+  genderRestriction: string;
+  autoExitGracePeriod: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface TeamItem {
+  id: number;
+  name: string;
+  description: string;
+  visibilitySetting: string;
+  genderRestriction: string;
+  autoExitGracePeriod: number;
+  createdAt: Date;
+  updatedAt: Date;
+  users: {
+    id: number;
+    name: string;
+    image: string;
+  }[];
+}
