78699d00fb289277e3195c4617cc64aee40a11d0
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-10-10 15:14:43 +0900

    Add: TeamItem コンポーネントを追加する

---
+++ app/src/components/molecules/Item/TeamItem/index.tsx
@@ -0,0 +1,58 @@
+import {makeStyles, Theme} from '@material-ui/core/styles';
+import {TeamItem} from '@/interfaces/models/team';
+import {Card, Box, Typography, Avatar, CardContent} from '@material-ui/core';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    maxWidth: 500,
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  main: {
+    background: theme.palette.primary.main,
+  },
+  title: {
+    display: 'flex',
+    margin: 'auto',
+    color: '#fff',
+  },
+  avatarGroup: {
+    justifyContent: 'center',
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(1),
+  },
+  avatar: {
+    width: 48,
+    height: 48,
+  },
+  cardContent: {
+    paddingBottom: theme.spacing(1),
+  },
+}));
+
+type ListPropsType = {
+  team: TeamItem;
+};
+
+const TeamItem = ({team}: ListPropsType) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card} key={team.id}>
+      <Box className={classes.main} minHeight={75} display={'flex'}>
+        <Typography className={classes.title}>{team.name}</Typography>
+      </Box>
+      {team.users && (
+        <AvatarGroup className={classes.avatarGroup} max={5}>
+          {team.users.map((user) => (
+            <Avatar className={classes.avatar} />
+          ))}
+        </AvatarGroup>
+      )}
+      <CardContent className={classes.cardContent}>{team.description}</CardContent>
+    </Card>
+  );
+};
+
+export default TeamItem;
