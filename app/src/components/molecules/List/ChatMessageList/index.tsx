396cf2a5c2a3eb6146671709fcf0746c35a5c788
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2022-02-11 21:15:00 +0900

    Add: ChatMessageList コンポーネントを追加する

---
+++ app/src/components/molecules/List/ChatMessageList/index.tsx
@@ -0,0 +1,36 @@
+import {TeamMessageItem} from '@/interfaces/models/teamMessage';
+import {User} from '@/interfaces/models/user';
+import MyChatMessage from '../../Message/MyChatMessage';
+import OtherChatMessage from '../../Message/OherChatMessage';
+import moment from 'moment';
+
+type ListPropsType = {
+  teamMessageList: TeamMessageItem[] | [];
+  currentUser: User;
+};
+
+const ChatMessageList = ({teamMessageList, currentUser}: ListPropsType) => {
+  return (
+    <>
+      {teamMessageList.map((teamMessage: TeamMessageItem, index: number) => (
+        <>
+          {teamMessage.user.id == currentUser.id ? (
+            <MyChatMessage
+              message={teamMessage.content}
+              timestamp={moment(teamMessage.createdAt).format('YYYY年MM月DD日 HH:mm')}
+            />
+          ) : (
+            <OtherChatMessage
+              message={teamMessage.content}
+              timestamp={moment(teamMessage.createdAt).format('YYYY年MM月DD日 HH:mm')}
+              photoURL={teamMessage.user.image.url}
+              displayName={teamMessage.user.name}
+            />
+          )}
+        </>
+      ))}
+    </>
+  );
+};
+
+export default ChatMessageList;
