c2f365fb3667edaf537d4572308cdff1757978ee
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2022-02-28 09:56:29 +0900

    Add: 投稿フォームに画像プレビュー機能を追加する

--- app/src/components/molecules/Form/MicropostForm/index.tsx
+++ app/src/components/molecules/Form/MicropostForm/index.tsx
@@ -1,5 +1,5 @@
 import {makeStyles, Theme} from '@material-ui/core/styles';
-import {Dispatch, SetStateAction, useState, useContext} from 'react';
+import {Dispatch, SetStateAction, useState, useContext, useCallback} from 'react';
 import {MicropostItem} from '@/interfaces/models/micropost';
 import {AuthContext} from '@/pages/_app';
 import {createMicropostParams} from '@/interfaces';
@@ -10,6 +10,7 @@ import ImageIcon from '@material-ui/icons/Image';
 const useStyles = makeStyles((theme: Theme) => ({
   card: {
     borderRadius: 0,
+    maxHeight: 1000,
     maxWidth: 500,
   },
   avatar: {
@@ -17,13 +18,27 @@ const useStyles = makeStyles((theme: Theme) => ({
     height: 40,
     margin: theme.spacing(1),
   },
+  contentBox: {
+    flexDirection: 'column',
+    width: '100%',
+  },
+  content: {
+    border: 'none',
+  },
+  preview: {
+    height: '100%',
+    width: '100%',
+  },
   toolBar: {
     marginTop: theme.spacing(1),
     marginLeft: theme.spacing(7),
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  image: {
+  input: {
+    display: 'none',
+  },
+  imageUploadIcon: {
     cursor: 'pointer',
   },
   btn: {},
@@ -38,10 +53,21 @@ const MicropostForm = ({micropostList, setMicropostList}: FormPropsType) => {
   const classes = useStyles();
   const [content, setContent] = useState<string>('');
   const {isLoggedIn, currentUser} = useContext(AuthContext);
+  const [image, setImage] = useState<File>();
+  const [preview, setPreview] = useState<string>('');
 
   const handleChnageContent = (e) => {
     setContent(e.target.value);
   };
+
+  const uploadImage = useCallback((e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  }, []);
+  const previewImage = useCallback((e) => {
+    const file = e.target.files[0];
+    setPreview(window.URL.createObjectURL(file));
+  }, []);
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -67,20 +93,38 @@ const MicropostForm = ({micropostList, setMicropostList}: FormPropsType) => {
         <Card className={classes.card} variant="outlined">
           <CardContent>
             <form noValidate autoComplete="off">
-              <Box display={'flex'}>
+              <Box className={classes.content} display={'flex'}>
                 <Avatar className={classes.avatar} src={currentUser.image.url} />
-                <TextField
-                  value={content}
-                  multiline
-                  rows={4}
-                  fullWidth
-                  margin="dense"
-                  onChange={handleChnageContent}
-                  placeholder="今日の目標を宣言しよう"
-                />
+                <Box className={classes.contentBox} display={'flex'}>
+                  <TextField
+                    value={content}
+                    multiline
+                    rows={4}
+                    fullWidth
+                    margin="dense"
+                    onChange={handleChnageContent}
+                    placeholder="今日の目標を宣言しよう"
+                    InputProps={{
+                      disableUnderline: true,
+                    }}
+                  />
+                  {preview ? <img className={classes.preview} src={preview} /> : <></>}
+                </Box>
               </Box>
               <Box className={classes.toolBar} display={'flex'}>
-                <ImageIcon className={classes.image} color="primary" fontSize="medium" />
+                <label htmlFor="icon-button-file">
+                  <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="icon-button-file"
+                    type="file"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      uploadImage(e);
+                      previewImage(e);
+                    }}
+                  />
+                  <ImageIcon className={classes.imageUploadIcon} color="primary" fontSize="medium" />
+                </label>
                 <Button
                   type="submit"
                   variant="contained"
